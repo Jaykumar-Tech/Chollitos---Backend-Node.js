892dcf8150bd114d98745f3ae506ae4740c1b400
gracephilipse <jaykumar930703@gmail.com>
2023-08-29 11:01:12 +0800

    Store get with cnt deal and discount

--- backend/models/store.model.js
+++ backend/models/store.model.js
@@ -33,7 +33,21 @@ Store.edit = (id, data) => {
 
 Store.get = (id) => {
     return new Promise((resolve, reject) => {
-        client.query("SELECT * FROM store WHERE id=?", [id], (err, rows) => {
+        client.query(`SELECT store.*, D.cnt_deal , S.cnt_discount
+        FROM store
+        LEFT JOIN
+        (SELECT count(store_id) as cnt_deal, store_id
+        FROM deal
+        WHERE type="deal"
+        GROUP BY store_id) D
+        ON D.store_id=store.id 
+        LEFT JOIN
+        (SELECT count(store_id) as cnt_discount, store_id
+        FROM deal
+        WHERE type!="deal"
+        GROUP BY store_id) S
+        ON S.store_id=store.id 
+        WHERE store.id=?`, [id], (err, rows) => {
             if (err) {
                 reject(err);
                 return;
