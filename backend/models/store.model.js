e6fc8fe8ca641383704a9f92863159a7348cfcd1
gracephilipse <jaykumar930703@gmail.com>
2023-08-29 23:30:52 +0800

    store get by name with count of deal

--- backend/models/store.model.js
+++ backend/models/store.model.js
@@ -6,28 +6,28 @@ const Store = function (store) {
 };
 
 Store.create = (data) => {
-    return new Promise((resolve, reject)=>{
+    return new Promise((resolve, reject) => {
         client.query("INSERT INTO store SET ?",
-        data, (err, row) => {
-            if (err) {
-                reject(err);
-                return;
-            }
-            resolve(row);
-        });
+            data, (err, row) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(row);
+            });
     })
 };
 
 Store.edit = (id, data) => {
-    return new Promise((resolve, reject)=>{
+    return new Promise((resolve, reject) => {
         client.query("UPDATE store SET ? WHERE id=?",
-        [data, id], (err, row) => {
-            if (err) {
-                reject(err);
-                return;
-            }
-            resolve(row);
-        });
+            [data, id], (err, row) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(row);
+            });
     })
 };
 
@@ -51,9 +51,9 @@ Store.get = (id) => {
             if (err) {
                 reject(err);
                 return;
-            } 
-            if ( rows.length == 0 ) {
-                reject({message: "Store Not Found"})
+            }
+            if (rows.length == 0) {
+                reject({ message: "Store Not Found" })
                 return
             }
             resolve(rows[0]);
@@ -67,9 +67,9 @@ Store.getAll = () => {
             if (err) {
                 reject(err);
                 return;
-            } 
-            if ( rows.length == 0 ) {
-                reject({message: "not found"})
+            }
+            if (rows.length == 0) {
+                reject({ message: "not found" })
                 return
             }
             resolve(rows);
@@ -79,18 +79,32 @@ Store.getAll = () => {
 
 Store.getByName = (name) => {
     return new Promise((resolve, reject) => {
-        client.query("SELECT * FROM store WHERE name=?",[name],
-         (err, rows) => {
-            if (err) {
-                reject(err);
-                return;
-            } 
-            if ( rows.length == 0 ) {
-                reject({message: "Store Not Found"})
-                return
-            }
-            resolve(rows[0]);
-        });
+        client.query(`SELECT store.*, D.cnt_deal , S.cnt_discount
+        FROM store
+        LEFT JOIN
+        (SELECT count(store_id) as cnt_deal, store_id
+        FROM deal
+        WHERE type="deal"
+        GROUP BY store_id) D
+        ON D.store_id=store.id 
+        LEFT JOIN
+        (SELECT count(store_id) as cnt_discount, store_id
+        FROM deal
+        WHERE type!="deal"
+        GROUP BY store_id) S
+        ON S.store_id=store.id 
+        WHERE store.name=?`, [name],
+            (err, rows) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                if (rows.length == 0) {
+                    reject({ message: "Store Not Found" })
+                    return
+                }
+                resolve(rows[0]);
+            });
     });
 };
 
