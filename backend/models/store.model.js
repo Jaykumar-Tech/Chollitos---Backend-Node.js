d2b16ebab6cb6486910587ea4d964f0d6bc9f4bb
gracephilipse <jaykumar930703@gmail.com>
2023-09-01 01:35:57 +0800

    vip store count

--- backend/models/store.model.js
+++ backend/models/store.model.js
@@ -78,22 +78,24 @@ Store.getAll = () => {
 };
 
 Store.getByName = (name) => {
+    const vip = name.split("_").length > 1;
+    name = name.split("_")[0] ;
     return new Promise((resolve, reject) => {
         client.query(`SELECT store.*, D.cnt_deal , S.cnt_discount
         FROM store
         LEFT JOIN
         (SELECT count(store_id) as cnt_deal, store_id
         FROM deal
-        WHERE type="deal"
+        WHERE type="deal" ${!vip? " AND vip=0": ""}
         GROUP BY store_id) D
         ON D.store_id=store.id 
         LEFT JOIN
         (SELECT count(store_id) as cnt_discount, store_id
         FROM deal
-        WHERE type!="deal"
+        WHERE type!="deal" ${!vip? " AND vip=0": ""}
         GROUP BY store_id) S
         ON S.store_id=store.id 
-        WHERE store.name=?`, [name],
+        WHERE store.name=?`, [name, vip],
             (err, rows) => {
                 if (err) {
                     reject(err);
