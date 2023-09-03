6e0aaff077493fb623acd81fbe355fab1448c9ed
gracephilipse <jaykumar930703@gmail.com>
2023-09-03 22:32:34 +0800

    vip access token test

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -2,31 +2,56 @@ const cacheUtil = require('../utils/cache.util');
 const jwtUtil = require('../utils/jwt.util');
 
 module.exports = async (req, res, next) => {
-    
     let token = req.headers.authorization;
     if (token && token.startsWith('Bearer ')) {
         token = token.slice(7, token.length);
     }
-
-    if (token) {
-        try {
-            token = token.trim();
-            /* ---------------------- Check For Blacklisted Tokens ---------------------- */
-            const isBlackListed = await cacheUtil.get(token);
-            if (isBlackListed) {
-                return res.status(401).json({ message: 'Unauthorized' });
+    if (req.method == "POST" && req.url == "/deal/find") {
+        if ( req.body.vip && req.body.vip > 0 ) {
+            if (token) {
+                try {
+                    token = token.trim();
+                    /* ---------------------- Check For Blacklisted Tokens ---------------------- */
+                    const isBlackListed = await cacheUtil.get(token);
+                    if (isBlackListed) {
+                        return res.status(401).json({ message: 'Unauthorized' });
+                    }
+                    const decoded = await jwtUtil.verifyToken(token);
+                    req.user = decoded;
+                    req.token = token;
+                    if ( req.user.role != "vip" ) res.status(401).json({ message: 'Unauthorized' });
+                    else next();
+                } catch (error) {
+                    return res.status(401).json({ message: 'Unauthorized' });
+                }
+            } else {
+                return res.status(400).json({ message: 'You are not allowed to get VIP bargains' })
             }
-            
-            const decoded = await jwtUtil.verifyToken(token);
-            req.user = decoded;
-            if ( req.method=="POST")
-                req.body.user_id = req.user.id;
-            req.token = token;
-            next();
-        } catch (error) {
-            return res.status(401).json({ message: 'Unauthorized' });
+        } else {
+            next() ;
         }
     } else {
-        return res.status(400).json({ message: 'Authorization header is missing.' })
+
+        if (token) {
+            try {
+                token = token.trim();
+                /* ---------------------- Check For Blacklisted Tokens ---------------------- */
+                const isBlackListed = await cacheUtil.get(token);
+                if (isBlackListed) {
+                    return res.status(401).json({ message: 'Unauthorized' });
+                }
+
+                const decoded = await jwtUtil.verifyToken(token);
+                req.user = decoded;
+                if (req.method == "POST")
+                    req.body.user_id = req.user.id;
+                req.token = token;
+                next();
+            } catch (error) {
+                return res.status(401).json({ message: 'Unauthorized' });
+            }
+        } else {
+            return res.status(400).json({ message: 'Authorization header is missing.' })
+        }
     }
 }
