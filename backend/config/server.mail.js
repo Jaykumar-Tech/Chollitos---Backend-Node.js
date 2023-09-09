718e4e5f281b15db5ea58833f4ed02713cf1e3ca
gracephilipse <jaykumar930703@gmail.com>
2023-09-09 12:57:22 +0800

    temp

--- backend/config/server.mail.js
+++ backend/config/server.mail.js
@@ -1,6 +1,8 @@
 module.exports = { 
-    mail: process.env.SERVER_MAIL,
-    password: process.env.SERVER_MAIL_PASSWORD,
+    // mail: process.env.SERVER_MAIL,
+    // password: process.env.SERVER_MAIL_PASSWORD,
+    mail: "daltonchua@outlook.com",
+    password: "dalton123",
     gmail: process.env.SERVER_GMAIL,
     gmail_password: process.env.SERVER_GMAIL_PASSWORD,
 }
