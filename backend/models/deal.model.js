9b05a28da0d6f8ed131392aa654d1dc98ccf89c3
gracephilipse <jaykumar930703@gmail.com>
2023-09-13 10:22:12 -0400

    temp

--- backend/models/deal.model.js
+++ backend/models/deal.model.js
@@ -295,14 +295,14 @@ Deal.delete = (id) => {
 
 Deal.setVipStatus = (id, status) => {
     return new Promise((resolve, reject) => {
-        client.query("UPDATE user SET vip=? WHERE id=?",
+        client.query("UPDATE deal SET vip=? WHERE id=?",
             [status, id], (err, row) => {
                 if (err) {
                     reject(err);
                     return;
                 } else if ( row.affectedRows == 0 ) {
                     reject({
-                        message: "User not found"
+                        message: "Deal not found"
                     })
                 }
                 else {
