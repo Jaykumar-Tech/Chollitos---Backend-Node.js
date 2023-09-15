0768b006f97e15cbf06e221a047fa30694ab5b45
gracephilipse <jaykumar930703@gmail.com>
2023-09-14 20:10:36 -0400

    popular shops, popular categorreis config

--- backend/models/config.model.js
+++ backend/models/config.model.js
@@ -6,7 +6,7 @@ const Config = function (config) {
 
 Config.get = () => {
     return new Promise((resolve, reject)=>{
-        client.query("SELECT site_title, welcome_email, language FROM config WHERE id=1",
+        client.query("SELECT site_title, welcome_email, language, popular_shops, popular_categories FROM config WHERE id=1",
         (err, rows) => {
             if (err) {
                 reject(err);
