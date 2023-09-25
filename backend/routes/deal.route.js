1207e9b1d6cfb25684b8f54aa1c7893b07f1dd17
gracephilipse <jaykumar930703@gmail.com>
2023-09-25 13:18:12 -0400

    category, store, upload image

--- backend/routes/deal.route.js
+++ backend/routes/deal.route.js
@@ -1,14 +1,13 @@
 const express = require('express');
-const router = express.Router();
 const multer = require("multer")
+const router = express.Router();
 
 const DealController = require('../controllers/deal.controller');
 const ErrorHandler = require('../middleware/error.middleware');
-const Exist = require("../middleware/existance.middleware")
 const schema = require('../validations/deal.validation');
 const validate = require('../utils/validator.util');
-const {dealFilter, dealCreate} = require("../middleware/deal.middleware");
-const {AuthGuard, AdminGuard} = require('../middleware/auth.middleware'); 
+const { dealFilter, dealCreate } = require("../middleware/deal.middleware");
+const { AuthGuard, AdminGuard } = require('../middleware/auth.middleware');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -22,16 +21,16 @@ const upload = multer({
     storage: storage
 });
 
-router.post('/upload', AuthGuard, upload.single('file'),  ErrorHandler(DealController.upload));
+router.post('/upload', AuthGuard, upload.single('file'), ErrorHandler(DealController.upload));
 router.post('/add', AuthGuard, dealCreate, validate(schema.add), ErrorHandler(DealController.create));
 router.post('/edit', AuthGuard, validate(schema.edit), ErrorHandler(DealController.edit));
 router.post('/find', dealFilter, AuthGuard, validate(schema.find), ErrorHandler(DealController.find));
 router.post('/count', dealFilter, AuthGuard, validate(schema.count), ErrorHandler(DealController.count));
-router.get('/get/:id',  AuthGuard,  ErrorHandler(DealController.get));
+router.get('/get/:id', AuthGuard, ErrorHandler(DealController.get));
 router.get('/usecode/:id', ErrorHandler(DealController.useCode));
-router.get('/delete/:id',      AdminGuard,           ErrorHandler(DealController.delete)); // register with email and password
-router.get('/activate/:id',     AdminGuard,            ErrorHandler(DealController.activate)); // register with email and password
-router.get('/deactivate/:id',   AdminGuard,              ErrorHandler(DealController.deactivate)); // register with email and password
+router.get('/delete/:id', AdminGuard, ErrorHandler(DealController.delete)); // register with email and password
+router.get('/activate/:id', AdminGuard, ErrorHandler(DealController.activate)); // register with email and password
+router.get('/deactivate/:id', AdminGuard, ErrorHandler(DealController.deactivate)); // register with email and password
 router.get('/set_vip/:id', AdminGuard, ErrorHandler(DealController.setVip));
 router.get('/unset_vip/:id', AdminGuard, ErrorHandler(DealController.unsetVip));
 router.get('/getall', AuthGuard, ErrorHandler(DealController.getAll))
